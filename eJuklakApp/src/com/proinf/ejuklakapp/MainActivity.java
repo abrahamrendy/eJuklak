94a2c085149a90769cba79d1fdf6b033ce1de550
Jefvin <jefvin94@gmail.com>
2015-03-10 16:35:19 +0700

    Navigasi
    Selesei semua navigasi tinggal timpa HTML dan rapihin CSS

--- eJuklakApp/src/com/proinf/ejuklakapp/MainActivity.java
+++ eJuklakApp/src/com/proinf/ejuklakapp/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
@@ -23,7 +24,11 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener{
+	
+	private Button[] listButtons;
+	private String urls[];
+	private WebView view;
 	
 	private Button createButton(String text){
 		Button b = new Button(this);
@@ -36,6 +41,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         /*ScrollView sv = new ScrollView(this);
         //sv.
         LinearLayout listButton = new LinearLayout(this);
@@ -48,9 +54,15 @@ public class MainActivity extends Activity {
         sv.addView(listButton);*/
         //listButton.set
         //listButton.setScr
-        WebView view = new WebView(this);
-        view.loadUrl("file:///android_asset/bab1.html");
-        
+        view = new WebView(this);
+        view.loadUrl("file:///android_asset/katapengantar.html");
+        urls = new String[6];
+        urls[0] = "file:///android_asset/katapengantar.html";
+        urls[1] = "file:///android_asset/bab1.html";
+        urls[2] = "file:///android_asset/bab2.html";
+        urls[3] = "file:///android_asset/bab3.html";
+        urls[4] = "file:///android_asset/bab4.html";
+        urls[5] = "file:///android_asset/lampiran.html";
         //view.setPadding(0, 0, 0, 0);
         //view.
         //setContentView(view);
@@ -67,16 +79,37 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         GridLayout parentLayout = ((GridLayout)findViewById(R.id.parentLayout));
         LinearLayout listButton = (LinearLayout)findViewById(R.id.tracks);
-        listButton.addView(createButton("Kata Pengantar"));//kata pengantar
+        listButtons = new Button[6];
+        listButtons[0] = createButton("Kata Pengantar");
+        listButtons[1] = createButton("Bab 1");
+        listButtons[2] = createButton("Bab 2");
+        listButtons[3] = createButton("Bab 3");
+        listButtons[4] = createButton("Bab 4");
+        listButtons[5] = createButton("Lampiran");
+        /*listButton.addView();//kata pengantar
         listButton.addView(createButton("Bab 1"));//bab 1
         listButton.addView(createButton("Bab 2"));//bab 2
         listButton.addView(createButton("Bab 3"));//bab 3
         listButton.addView(createButton("Bab 4"));//bab 4
-        listButton.addView(createButton("Lampiran"));//lampiran
+        listButton.addView(createButton("Lampiran"));//lampiran*/
+        for(int i=0;i<listButtons.length;i++){
+        	listButtons[i].setOnClickListener(this);
+        	listButton.addView(listButtons[i]);
+        }
         //parentLayout.addView(sv);
         parentLayout.addView(view);
         
         //TextView tv = ((TextView) findViewById(R.id.textView1));
         //tv.setText(Html.fromHtml("<html>alalala</html>"));
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<listButtons.length;i++){
+			if(v==listButtons[i]){
+				view.loadUrl(urls[i]);
+			}
+		}
+	}
 }
